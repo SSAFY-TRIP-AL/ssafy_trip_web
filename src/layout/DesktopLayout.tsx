d88123685cc0a36e7ee37f47512852bbb7e9e0b7
안현석 <aragorn@naver.com>
2026-06-05 16:31:57 +0900

    feat: 공통 footer 구현

--- src/layout/DesktopLayout.tsx
+++ src/layout/DesktopLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "../style.css";
 import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 export default function DesktopLayout() {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
